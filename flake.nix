43c0408cff714959a18f8ecafd9378e653c492e3
Haf <haf@protonmail.ch>
2024-06-03 19:24:30 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Following Kerninghan & Ritchie C Programming Language Book";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, utils, nixpkgs }: utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = with pkgs; mkShell {
+        buildInputs = [
+          clang_18
+        ];
+      };
+    }
+  );
+}
