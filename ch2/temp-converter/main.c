de8fda4ceb8ad60a0a088084b93f19b9603991c6
Haf <haf@protonmail.ch>
2024-06-03 20:15:35 +0200

    naisu

--- ch2/temp-converter/main.c
+++ ch2/temp-converter/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 // Checks whether the given string is a natural number
 bool str_is_nat(char* str) {
@@ -14,39 +15,38 @@ bool str_is_nat(char* str) {
   return true;
 }
 
+int get_int_arg(int argc, char* argv[], char* name, int default_value) {
+  int flag_pos = -1;
+
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], name) == 0) {
+      flag_pos = i;
+    }
+  }
+
+  if (flag_pos == -1) {
+    return default_value; 
+  }
+
+  int value_pos = flag_pos + 1;
+  if (value_pos >= argc) {
+    // TODO: There's no value, panic!
+    return default_value;
+  }
+
+  char* value = argv[value_pos];
+  if (!str_is_nat(value)) {
+    // TODO: Same! Also panic!!!
+    return default_value;
+  }
+
+  return atoi(value);
+}
+
 int main(int argc, char* argv[]) {
-  int lower = 0;
-  if (argc >= 1) {
-    char* arg = argv[1];
-    if (str_is_nat(arg)) {
-      lower = atoi(argv[1]);
-    } else {
-      printf("The string %s is not a natural number\n", arg);
-      return -1;
-    }
-  }
-
-  int upper = 300;
-  if (argc >= 2) {
-    char* arg = argv[2];
-    if (str_is_nat(arg)) {
-      upper = atoi(argv[2]);
-    } else {
-      printf("The string %s is not a natural number\n", arg);
-      return -1;
-    }
-  }
-
-  int step = 20;
-  if (argc >= 3) {
-    char* arg = argv[3];
-    if (str_is_nat(arg)) {
-      step = atoi(argv[3]);
-    } else {
-      printf("The string %s is not a natural number\n", arg);
-      return -1;
-    }
-  }
+  int lower = get_int_arg(argc, argv, "lower", 0);
+  int upper = get_int_arg(argc, argv, "upper", 300);
+  int step = get_int_arg(argc, argv, "step", 20);
 
   for (float fahr = lower; fahr <= upper; fahr += step) {
     float celsius = (5.0/9.0) * (fahr-32.0);
