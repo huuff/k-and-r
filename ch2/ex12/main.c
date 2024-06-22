6d5c625ecbf73224e487e6354173e28f9bc0a677
Haf <haf@protonmail.ch>
2024-06-22 20:33:29 +0200

    DRYed it

--- ch2/ex12/main.c
+++ ch2/ex12/main.c
@@ -7,6 +7,8 @@ struct current_word {
   char buffer[64];
 };
 
+void print_current_word(struct current_word *current_word);
+
 int main() {
   bool inside_word = false;
   struct current_word current_word = {0};
@@ -14,21 +16,23 @@ int main() {
   int c;
   while ((c = getchar()) != EOF) {
     if (isblank(c) || c == '\n') {
-      if (current_word.length > 0) {
-        current_word.buffer[current_word.length] = '\0';
-        printf("%s\n", current_word.buffer);
-      }
-      current_word.length = 0;
+      print_current_word(&current_word);
     } else {
       current_word.buffer[current_word.length++] = (char)c;
     }
   }
 
-  // TODO: Extract this behavior to a function?
   if (current_word.length > 0) {
-    current_word.buffer[current_word.length] = '\0';
-    printf("%s", current_word.buffer);
+    print_current_word(&current_word);
   }
 
   return 0;
+}
+
+void print_current_word(struct current_word *current_word) {
+  if (current_word->length > 0) {
+    current_word->buffer[current_word->length] = '\0';
+    printf("%s\n", current_word->buffer);
+    current_word->length = 0;
+  }
 }
