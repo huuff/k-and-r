402875a8b34c18a06cfa7d0f631f1659f2d02b0c
Haf <haf@protonmail.ch>
2024-06-23 12:22:10 +0200

    not quite satisfying the spec

---
+++ ch2/ex13/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+// Surely some random german word is this long
+#define MAX_WORD_LENGTH 128
+
+struct word_length_bounds {
+  int min;
+  int max;
+};
+struct word_length_bounds find_word_length_bounds(int *word_lengths);
+void print_lengths_plaintext(int *word_lengths,
+                             struct word_length_bounds *bounds);
+
+int main() {
+  int word_lengths[MAX_WORD_LENGTH];
+
+  for (int i = 0; i < MAX_WORD_LENGTH; ++i) {
+    word_lengths[i] = 0;
+  }
+
+  int current_word_length = 0;
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\n' || c == ' ' || c == '\t') {
+      if (current_word_length > 0) {
+        word_lengths[current_word_length]++;
+        current_word_length = 0;
+      }
+    } else {
+      current_word_length++;
+    }
+  }
+
+  // A final pass for the last word if it's not followed by a blank
+  if (current_word_length > 0) {
+    word_lengths[current_word_length]++;
+  }
+
+  struct word_length_bounds word_length_bounds =
+      find_word_length_bounds(word_lengths);
+
+  if (word_length_bounds.min == -1 || word_length_bounds.max == -1) {
+    fprintf(stderr, "No words found!\n");
+  }
+
+  print_lengths_plaintext(word_lengths, &word_length_bounds);
+
+  return 0;
+}
+
+void print_lengths_plaintext(int *word_lengths,
+                             struct word_length_bounds *bounds) {
+
+  for (int i = bounds->min; i <= bounds->max; ++i) {
+    printf("Length %d: %d\n", i, word_lengths[i]);
+  }
+}
+
+// Finds the minimum and maximum index that has a count larger than 0,
+// to decide what axis bounds to show on the histogram
+struct word_length_bounds find_word_length_bounds(int *word_lengths) {
+  struct word_length_bounds word_length_bounds = {-1, -1};
+
+  for (int i = 0; i < MAX_WORD_LENGTH; ++i) {
+    if (word_lengths[i] > 0) {
+      word_length_bounds.min = i;
+      break;
+    }
+  }
+
+  for (int i = 0; i < MAX_WORD_LENGTH; i++) {
+    if (word_lengths[i] > word_length_bounds.max) {
+      word_length_bounds.max = i;
+    }
+  }
+
+  return word_length_bounds;
+}
